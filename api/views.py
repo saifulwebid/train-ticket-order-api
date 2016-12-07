e08e5ecb603b8482319dba8b8685b4f9412356dd
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-12-07 14:18:35 +0700

    Using mixins http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-mixins

--- api/views.py
+++ api/views.py
@@ -2,47 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import mixins, generics
 from .models import *
 from .serializers import *
-from django.http import Http404
 
-class StasiunList(APIView):
-	def get(self, request, format=None):
-		stasiun = Stasiun.objects.all()
-		serializer = StasiunSerializer(stasiun, many=True)
-		return Response(serializer.data)
+class StasiunList(generics.GenericAPIView,
+		mixins.ListModelMixin,
+		mixins.CreateModelMixin):
+	queryset = Stasiun.objects.all()
+	serializer_class = StasiunSerializer
 
-	def post(self, request, format=None):
-		serializer = StasiunSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	def get(self, request, *args, **kwargs):
+		return self.list(request, *args, **kwargs)
 
-class StasiunDetail(APIView):
-	def get_object(self, pk):
-		try:
-			return Stasiun.objects.get(pk=pk)
-		except Stasiun.DoesNotExist:
-			raise Http404
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
 
-	def get(self, request, pk, format=None):
-		stasiun = self.get_object(pk)
-		serializer = StasiunSerializer(stasiun)
-		return Response(serializer.data)
+class StasiunDetail(generics.GenericAPIView,
+		mixins.RetrieveModelMixin,
+		mixins.UpdateModelMixin,
+		mixins.DestroyModelMixin):
+	queryset = Stasiun.objects.all()
+	serializer_class = StasiunSerializer
 
-	def post(self, request, pk, format=None):
-		stasiun = self.get_object(pk)
-		serializer = StasiunSerializer(stasiun, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	def get(self, request, *args, **kwargs):
+		return self.retrieve(request, *args, **kwargs)
 
-	def delete(self, request, pk, format=None):
-		stasiun = self.get_object(pk)
-		stasiun.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+	def post(self, request, *args, **kwargs):
+		return self.update(request, *args, **kwargs)
+
+	def delete(self, request, *args, **kwargs):
+		return self.destroy(request, *args, **kwargs)
