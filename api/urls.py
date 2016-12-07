aa7e8f980aa664ddf2a3e2d243614b6400d1f570
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-12-07 13:26:45 +0700

    Utilize REST Framework's Request and Response http://www.django-rest-framework.org/tutorial/2-requests-and-responses/

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
 	url(r'^stasiun/$', views.stasiun_list),
 	url(r'^stasiun/(?P<pk>[A-Z]+)/$', views.stasiun_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -2,50 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
-class JSONResponse(HttpResponse):
-	def __init__(self, data, **kwargs):
-		content = JSONRenderer().render(data)
-		kwargs['content_type'] = 'application/json'
-		super(JSONResponse, self).__init__(content, **kwargs)
-
-def stasiun_list(request):
+@api_view(['GET', 'POST'])
+def stasiun_list(request, format=None):
 	if request.method == 'GET':
 		stasiun = Stasiun.objects.all()
 		serializer = StasiunSerializer(stasiun, many=True)
-		return JSONResponse(serializer.data)
+		return Response(serializer.data)
 
 	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = StasiunSerializer(data=data)
+		serializer = StasiunSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JSONResponse(serializer.data, status=201)
-		return JSONResponse(serializer.errors, status=400)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def stasiun_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def stasiun_detail(request, pk, format=None):
 	try:
 		stasiun = Stasiun.objects.get(pk=pk)
 	except Stasiun.DoesNotExist:
-		return HttpResponse(status=404)
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
 	if request.method == 'GET':
 		serializer = StasiunSerializer(stasiun)
-		return JSONResponse(serializer.data)
+		return Response(serializer.data)
 
 	elif request.method == 'PUT':
-		data = JSONParser().parse(request)
-		serializer = StasiunSerializer(stasiun, data=data)
+		serializer = StasiunSerializer(stasiun, data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JSONResponse(serializer.data)
-		return JSONResponse(serializer.errors, status=400)
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	elif request.method == 'DELETE':
 		snippet.delete()
-		return HttpResponse(status=204)
+		return Response(status=status.HTTP_204_NO_CONTENT)
