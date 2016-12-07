fd25b84b3ced8239ce28bb30d3748556f4ed8594
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-12-07 17:38:16 +0700

    Change serializer definition order Because Python doesn't support forward declaration

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import *
 
-class BookingSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Booking
-		fields = '__all__'
-
 class KeretaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Kereta
@@ -14,6 +9,11 @@ class KeretaSerializer(serializers.ModelSerializer):
 class LayananKeretaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = LayananKereta
+		fields = '__all__'
+
+class TipePembayaranSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = TipePembayaran
 		fields = '__all__'
 
 class PembayaranSerializer(serializers.ModelSerializer):
@@ -31,6 +31,12 @@ class PenumpangSerializer(serializers.ModelSerializer):
 		model = Penumpang
 		fields = '__all__'
 
+class BookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = '__all__'
+		depth = 1
+
 class RangkaianPerjalananSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = RangkaianPerjalanan
@@ -39,10 +45,5 @@ class RangkaianPerjalananSerializer(serializers.ModelSerializer):
 class StasiunSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Stasiun
-		fields = '__all__'
-
-class TipePembayaranSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = TipePembayaran
 		fields = '__all__'
 
