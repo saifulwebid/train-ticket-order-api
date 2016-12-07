f5e7dcc2fce9f76e90e4917a42e6c7394a958228
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-12-07 12:52:29 +0700

    Create serializers for all models

---
+++ api/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import *
+
+class BookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = '__all__'
+
+class KeretaSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Kereta
+		fields = '__all__'
+
+class LayananKeretaSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = LayananKereta
+		fields = '__all__'
+
+class PembayaranSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Pembayaran
+		fields = '__all__'
+
+class PemesanSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Pemesan
+		fields = '__all__'
+
+class PenumpangSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Penumpang
+		fields = '__all__'
+
+class RangkaianPerjalananSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = RangkaianPerjalanan
+		fields = '__all__'
+
+class StasiunSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Stasiun
+		fields = '__all__'
+
+class TipePembayaranSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = TipePembayaran
+		fields = '__all__'
+
